67ee837d60eafdd47c3db140badd822f239ab634
Nikolaos_Voulgaris <nickvoul3@gmail.com>
2024-04-07 22:30:57 +0300

    Create Basic Main

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'member_search_page/member_search_page.dart';
+//import 'member_home_page/member_home_page.dart';
+import 'clubs_profile_info/guest_club_info.dart';
+
+void main() => runApp(const MainClass());
+
+class MainClass extends StatefulWidget {
+  const MainClass({super.key});
+
+  @override
+  State<MainClass> createState() => Main_Class();
+}
+
+class Main_Class extends State<MainClass> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MainRoute());
+  }
+}
+
+class MainRoute extends StatelessWidget {
+  const MainRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(330, 30),
+                  backgroundColor: const Color.fromRGBO(60, 111, 159, 1),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const ClubInfo()),
+                  );
+                },
+                child: const Text(
+                  "Club Profile",
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(330, 30),
+                  backgroundColor: const Color.fromRGBO(60, 111, 159, 1),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const MemberSearchPage()),
+                  );
+                },
+                child: const Text(
+                  "Member Search Page",
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
