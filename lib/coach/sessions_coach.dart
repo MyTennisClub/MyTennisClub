13a6e1fee95a7fcc08598187fd627a9d4634a13a
stamelosxp <stamelosxp@gmail.com>
2024-04-09 15:10:28 +0300

    Resolve critical bug impacting session element sizes.

--- lib/coach/sessions_coach.dart
+++ lib/coach/sessions_coach.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'current_card_coach.dart';
 import 'next_card_coach.dart';
 
@@ -17,113 +15,113 @@ class _SessionsCoachState extends State<SessionsCoach> {
 
   @override
   Widget build(BuildContext context) {
-    const items = 20;
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 3),
-          child: Row(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(right: 7),
-                child: SizedBox(
-                  height: 30,
-                  child: ElevatedButton(
-                    onPressed: () => {
-                      setState(() {
-                        currentIsPressed = true;
-                        nextIsPressed = false;
-                      })
-                    },
-                    child: Text(
-                      'Current',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor:
-                          currentIsPressed ? Color(0xFFFF3C70A0) : Colors.grey,
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 7),
-                child: SizedBox(
-                  height: 30,
-                  child: ElevatedButton(
-                    onPressed: () => {
-                      setState(() {
-                        currentIsPressed = false;
-                        nextIsPressed = true;
-                      })
-                    },
-                    child: Text(
-                      'Next',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor:
-                          nextIsPressed ? Color(0xFFFF3C70A0) : Colors.grey,
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-        Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: currentIsPressed ? Card1_Coach():Card2_Coach()),
-        Row(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 3),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(right: 14),
-                    child: SizedBox(
-                      height: 30,
-                      child: ElevatedButton(
-                        onPressed: () => {},
-                        child: Text(
-                          'Attendance Book',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFFFF3C70A0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                        ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 7),
+                  child: SizedBox(
+                    height: 30,
+                    child: ElevatedButton(
+                      onPressed: () => {
+                        setState(() {
+                          currentIsPressed = true;
+                          nextIsPressed = false;
+                        })
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                        currentIsPressed ? const Color(0xFFFF3C70A0) : Colors.grey,
+                      ),
+                      child: const Text(
+                        'Current',
+                        style: TextStyle(color: Colors.white),
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15),
-                    child: SizedBox(
-                      height: 30,
-                      child: ElevatedButton(
-                        onPressed: () => {},
-                        child: Text(
-                          'Attendance Book',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFFFF3C70A0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                        ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 7.0),
+                  child: SizedBox(
+                    height: 30,
+                    child: ElevatedButton(
+                      onPressed: () => {
+                        setState(() {
+                          currentIsPressed = false;
+                          nextIsPressed = true;
+                        })
+                      },
+                      child: const Text(
+                        'Next',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                        nextIsPressed ? const Color(0xFFFF3C70A0) : Colors.grey,
                       ),
                     ),
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             ),
-          ],
-        )
-      ],
+          ),
+          Container(
+            child: Expanded(
+              child: currentIsPressed ? Card1_Coach():const Card2_Coach(),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: Card(
+                  child: SizedBox(
+                    height: 35,
+                    child: ElevatedButton(
+                      onPressed: () => {},
+                      child: const Text(
+                        'Attendance Book',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFFF3C70A0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Card(
+                  child: SizedBox(
+                    height: 35,
+                    child: ElevatedButton(
+                      onPressed: () => {},
+                      child: const Text(
+                        'Training Program',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFFF3C70A0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
