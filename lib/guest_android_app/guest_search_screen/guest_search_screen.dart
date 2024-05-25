9857530311234d69468aace9a959a8df646f194a
Nikolaos_Voulgaris <83508203+NickVoulg02@users.noreply.github.com>
2024-05-25 20:03:12 +0300

    Guest Club Profile Tabs fixed

--- lib/guest_android_app/guest_search_screen/guest_search_screen.dart
+++ lib/guest_android_app/guest_search_screen/guest_search_screen.dart
@@ -2,29 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:mytennisclub/guest_android_app/guest_search_screen/guest_club_profile/guest_club_profile.dart';
 
 class GuestsSearchScreen extends StatefulWidget {
-  final Function checkIndex;
-  const GuestsSearchScreen({required this.checkIndex, super.key});
+  const GuestsSearchScreen({super.key});
 
   @override
   State<GuestsSearchScreen> createState() => _GuestsSearchScreenState();
 }
 
 class _GuestsSearchScreenState extends State<GuestsSearchScreen> {
+  bool profileSelected = false;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FilledButton(
-          onPressed: () async {
-            await Navigator.of(context)
-                .push(MaterialPageRoute(builder: (context) => const ClubInfo()))
-                .then((value) {
-              setState(() {
-                print(value);
-                widget.checkIndex(value);
-              });
-            });
-          },
-          child: const Text('Select Patras Tennis Club')),
-    );
+    return (!profileSelected)
+        ? Center(
+            child: FilledButton(
+                onPressed: () {
+                  setState(() {
+                    profileSelected = true;
+                  });
+                },
+                child: const Text('Select Patras Tennis Club')),
+          )
+        : const ClubProfile();
   }
 }
