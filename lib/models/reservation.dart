91ec02043a949beead775e9106c0f4ca53d076c3
thanasisbkls <bakalis.th@gmail.com>
2024-05-29 20:16:15 +0300

    added function to fetch a reservation based on id from the db

--- lib/models/reservation.dart
+++ lib/models/reservation.dart
@@ -1,7 +1,7 @@
 import 'dart:ffi';
 import 'package:flutter/material.dart';
-import 'package:mytennisclub/models/qr.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:mytennisclub/Database/ConnectionDatabase.dart';
 
 enum Type {
   valid,
@@ -33,4 +33,41 @@ class Reservation {
   final int courtId;
   final Bool equipment;
   late Widget qrCode;
+
+  static Future<Map<String, dynamic>> fetchReservationFromDb(int id) async {
+    Map<String, dynamic> reservationMap = {};
+    try {
+      final conn = await MySQLConnector.createConnection();
+      if (conn != null) {
+        var reservation = await conn.query(
+          'SELECT * FROM reservations WHERE id = ?',
+          [id],
+        );
+
+        if (reservation.isNotEmpty) {
+          var row = reservation.first;
+
+          print(reservation);
+
+          reservationMap = {
+            'id': row['id'],
+            'title': row['title'],
+            'court': row['court'],
+            'start_time': row['start_time'],
+            'end_time': row['end_time'],
+            'res_type': row['res_type'],
+          };
+
+          await conn.close();
+        } else {
+          await conn.close();
+          throw Exception('No reservation found with the given ID');
+        }
+      }
+    } catch (e) {
+      throw Exception('Error fetching reservation: $e');
+    }
+
+    return reservationMap;
+  }
 }
